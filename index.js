109116c319d04d791051b55f189c56774f02b3bd
rdftyguh8 <gs1alwayboy@hotmail.com>
2024-05-10 23:33:34 +0700

    init

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const pull = (arr, ...args) => {
-  let argState = Array.isArray(args[0]) ? args[0] : args;
-  let pulled = arr.filter((v, i) => !argState.includes(v));
-  arr.length = 0;
-  pulled.forEach((v) => arr.push(v));
-};
+console.log("hello world");
